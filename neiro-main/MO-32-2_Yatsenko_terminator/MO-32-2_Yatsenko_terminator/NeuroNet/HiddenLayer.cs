2beb7e058af3941a2aaba4204ebe3602c9370679
nikayacenko <yacenkonika05@mail.ru>
2025-10-11 15:31:19 +0300

    Add files via upload

---
+++ neiro-main/MO-32-2_Yatsenko_terminator/MO-32-2_Yatsenko_terminator/NeuroNet/HiddenLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MO_32_2_Yatsenko_terminator.NeuroNet
+{
+    class HiddenLayer:Layer
+    {
+        public HiddenLayer(int non, int nopn, NeuronType nt, string type) : base(non, nopn, nt, type) { 
+        }
+        public override void Recognize(Network net, Layer nextLayer)
+        {
+            double[] hidden_out = new double[numofneurons];
+            for(int i = 0; i < numofneurons; i++)
+            {
+                hidden_out[i] = neurons[i].Output;
+            }
+            nextLayer.Data = hidden_out; //передача выходного сигнала на вход следующего 
+        }
+
+        //обратный проход
+        public override double[] BackwardPass(double[] gr_sums)
+        {
+            double[] gr_sum = new double[numofprevneurons+1];
+
+            return gr_sum;
+        }
+    }
+}
