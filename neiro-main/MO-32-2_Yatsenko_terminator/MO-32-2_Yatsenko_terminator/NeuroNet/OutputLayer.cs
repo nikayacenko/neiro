ab1739c741eb25857537bcbacba257da9a06008c
nikayacenko <yacenkonika05@mail.ru>
2025-10-25 15:27:42 +0300

    Add files via upload

---
+++ neiro-main/MO-32-2_Yatsenko_terminator/MO-32-2_Yatsenko_terminator/NeuroNet/OutputLayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MO_32_2_Yatsenko_terminator.NeuroNet
+{
+    internal class OutputLayer : Layer
+    {
+        public OutputLayer(int non, int nopn, NeuronType nt, string type) : base(non, nopn, nt, type) { }
+
+        public override void Recognize(Network net, Layer nextLayer)
+        {
+            double e_sum = 0;
+            for (int i = 0; i < neurons.Length; i++)
+            {
+                e_sum += neurons[i].Output;
+            }
+
+            for (int i = 0; i < neurons.Length; i++)
+            {
+                net.Fact[i] = neurons[i].Output / e_sum;
+            }
+        }
+
+        public override double[] BackwardPass(double[] errors)
+        {
+            double[] gr_sum = new double[numofprevneurons + 1];
+            return gr_sum;
+        }
+    }
+}
